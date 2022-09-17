c2ebf58564d79369ae4563ee5d1e3f093282ffa5
Glenn <705252+MlennGarien@users.noreply.github.com>
2022-09-17 12:38:26 +0200

    Update price_helper.ts

--- src/helpers/price_helper.ts
+++ src/helpers/price_helper.ts
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-const COIN_ID = 'metal'
+const COIN_ID = 'metal-blockchain'
 const COINGECKO_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=metal&vs_currencies=usd'
 
 const coingeckoApi = axios.create({
@@ -10,7 +10,7 @@ const coingeckoApi = axios.create({
 
 export async function getAvaxPriceUSD(): Promise<number> {
     const res = await axios.get(COINGECKO_URL)
-    return res.data['metal']['usd']
+    return res.data['metal-blockchain']['usd']
 }
 
 let priceHistory: [number, number][] = []
